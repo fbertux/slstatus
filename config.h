b458b9b454e5b5131aef7b4f807e030f9fe37dd5
Fabio Berton <fbberton@gmail.com>
2020-11-08 22:34:58 -0300

    config.h: Show battery status
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- config.h
+++ config.h
@@ -63,5 +63,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
     /* function format          argument */
+    { battery_perc, " %s\% | ",  "BAT1"},
     { datetime, "%s",           "%F %H:%M" },
 };
